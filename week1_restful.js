74b6e77339817d16a800824b53543209431a52a4
neowu <neo_wu@mail.chungyo.net>
2019-05-21 16:06:02 +0800

    完成姓、名、電話驗證

--- week1_restful.js
+++ week1_restful.js
@@ -1,5 +1,4 @@
 axios.defaults.baseURL = "https://5cdab7b5eb39f80014a75933.mockapi.io/users";
-// Vue.use(VeeValidate);
 let app = new Vue({
     el: "#app",
     data: {
@@ -8,13 +7,21 @@ let app = new Vue({
             lastName: "",
             firstName: "",
             tel: "",
-            photo: ""
+            photo: "",
+            lastName_valid: false,
+            firstName_valid: false,
+            tel_valid: false,
+            photo_valid: false
         },
         userDataAdd: {
             lastName: "",
             firstName: "",
             tel: "",
-            photo: ""
+            photo: "",
+            lastName_valid: false,
+            firstName_valid: false,
+            tel_valid: false,
+            photo_valid: false
         },
         queryUserId: '',
         editFormToggle: false,
@@ -102,8 +109,11 @@ let app = new Vue({
                         jsonData.push(res.data[i]);
                     }
                     console.log(jsonData)
-                })
+                });
             this.userData = jsonData;
+            this.userDataAdd.firstName_valid = false;
+            this.userDataAdd.lastName_valid = false;
+            this.userDataAdd.tel_valid = false
         },
         deleteUser(index) {
             this.queryUserId = this.userData[index].id;
@@ -120,20 +130,26 @@ let app = new Vue({
         },
         editUser(index) {
             this.queryUserId = this.userData[index].id;
-            this.userDataUpdate.firstName = this.userData[index].firstName;
-            this.userDataUpdate.lastName = this.userData[index].lastName;
-            this.userDataUpdate.tel = this.userData[index].tel;
+            // this.userDataUpdate.firstName = this.userData[index].firstName;
+            // this.userDataUpdate.lastName = this.userData[index].lastName;
+            // this.userDataUpdate.tel = this.userData[index].tel;
             this.userDataUpdate.photo = this.userData[index].photo;
             this.editFormToggle = true;
             console.log(this.userData[index])
         },
-        editCancel() {
+        buttonCancel() {
             this.editFormToggle = false;
             this.addFormToggle = false;
-            let userDataUpdate = this.userDataUpdate;
-            userDataUpdate.lastName = "";
-            userDataUpdate.firstName = "";
-            userDataUpdate.tel = "";
+            let userDataAdd = this.userDataAdd;
+            let userDataUpdate = this.userDataUpdate
+            this.clearRemainData();
+            userDataAdd.lastName_valid = false;
+            userDataAdd.firstName_valid = false;
+            userDataAdd.tel_valid = false;
+            userDataUpdate.lastName_valid = false;
+            userDataUpdate.firstName_valid = false;
+            userDataUpdate.tel_valid = false;
+
         },
         uploadPhoto_AddUser(e) {
             let file = e.target.files[0];
@@ -154,53 +170,122 @@ let app = new Vue({
             reader.readAsDataURL(file);
         },
         editConfirm() {
-
-            axios.put("/" + this.queryUserId, {
-                firstName: this.userDataUpdate.firstName,
-                lastName: this.userDataUpdate.lastName,
-                tel: this.userDataUpdate.tel,
-                email: this.userDataUpdate.email,
-                photo: this.userDataUpdate.photo
-            })
-                .then(res => {
-                    console.log(res);
-                    this.userListReload();
-                    this.editFormToggle = false;
+            if (this.userDataUpdate.lastName_valid && this.userDataUpdate.firstName_valid && this.userDataUpdate.tel_valid) {
+                axios.put("/" + this.queryUserId, {
+                    firstName: this.userDataUpdate.firstName,
+                    lastName: this.userDataUpdate.lastName,
+                    tel: this.userDataUpdate.tel,
+                    email: this.userDataUpdate.email,
+                    photo: this.userDataUpdate.photo
                 })
-                .catch(err => {
-                    let errMassage;
-                    switch (err.response.status) {
-                        case 413 :
-                            errMassage = "上傳圖片過大!"
-                    }
-                    alert(errMassage);
-                })
+                    .then(res => {
+                        console.log(res);
+                        this.userListReload();
+                        this.clearRemainData();
+                        this.editFormToggle = false;
+                    })
+                    .catch(err => {
+                        let errMassage;
+                        switch (err.response.status) {
+                            case 413 :
+                                errMassage = "上傳圖片過大!"
+                        }
+                        alert(errMassage);
+                    })
+            } else {
+                alert("請確認填妥表單")
+            }
+        },
+        editUser_checkValid_lastName() {
+            let lastName = this.userDataUpdate.lastName;
+            let lastNameRegexp = "[\u4e00-\u9fa5]{0,5}";
+            let checkResult = lastName.match(lastNameRegexp);
+            console.log(checkResult);
+            if (checkResult[0] !== '' && checkResult.input.length <= 5) {
+                this.userDataUpdate.lastName_valid = true;
+            } else {
+                this.userDataUpdate.lastName_valid = false;
+            }
+        },
+        editUser_checkValid_firstName() {
+            let firstName = this.userDataUpdate.firstName;
+            let firstNameRegexp = "[\u4e00-\u9fa5]{0,5}";
+            let checkResult = firstName.match(firstNameRegexp);
+            if (checkResult[0] !== '' && checkResult.input.length <= 5) {
+                this.userDataUpdate.firstName_valid = true;
+            } else {
+                this.userDataUpdate.firstName_valid = false;
+            }
+        },
+        editUser_checkValid_tel() {
+            let tel = this.userDataUpdate.tel;
+            let telRegexp = "^09[0-9]{8}$";
+            let checkResult = tel.match(telRegexp);
+            if (checkResult[0] !== '' && checkResult.input.length <= 10) {
+                this.userDataUpdate.tel_valid = true;
+            } else {
+                this.userDataUpdate.tel_valid = false;
+            }
         },
         addUserConfirm() {
-            // alert('hi')
-            axios.post("", {
-                firstName: this.userDataAdd.firstName,
-                lastName: this.userDataAdd.lastName,
-                tel: this.userDataAdd.tel,
-                email: this.userDataAdd.email,
-                photo: this.userDataAdd.photo
-            })
-                .then(res => {
-                    console.log(res);
-                    this.checkPageNumbers();
-                    this.userListReload();
-                    this.clearAddData();
-                    this.addFormToggle = false;
+            if (this.userDataAdd.lastName_valid && this.userDataAdd.firstName_valid && this.userDataAdd.tel_valid) {
+                axios.post("", {
+                    firstName: this.userDataAdd.firstName,
+                    lastName: this.userDataAdd.lastName,
+                    tel: this.userDataAdd.tel,
+                    email: this.userDataAdd.email,
+                    photo: this.userDataAdd.photo
                 })
-                .catch(err => {
-                    console.log(err.response.status);
-                    let errMassage;
-                    switch (err.response.status) {
-                        case 413 :
-                            errMassage = "上傳圖片過大!"
-                    }
-                    alert(errMassage);
-                })
+                    .then(res => {
+                        console.log(res);
+                        this.checkPageNumbers();
+                        this.userListReload();
+                        this.clearRemainData();
+                        this.addFormToggle = false;
+                    })
+                    .catch(err => {
+                        console.log(err.response.status);
+                        let errMassage;
+                        switch (err.response.status) {
+                            case 413 :
+                                errMassage = "上傳圖片過大!"
+                        }
+                        alert(errMassage);
+                    })
+            } else {
+                alert("請確認填妥表單")
+            }
+        },
+        addUser_checkValid_lastName() {
+            let lastName = this.userDataAdd.lastName;
+            let lastNameRegexp = "[\u4e00-\u9fa5]{0,5}";
+            let checkResult = lastName.match(lastNameRegexp);
+            console.log(checkResult);
+            if (checkResult[0] !== '' && checkResult.input.length <= 5) {
+                this.userDataAdd.lastName_valid = true;
+            } else {
+                this.userDataAdd.lastName_valid = false;
+            }
+        },
+        addUser_checkValid_firstName() {
+            let firstName = this.userDataAdd.firstName;
+            let firstNameRegexp = "[\u4e00-\u9fa5]{0,5}";
+            let checkResult = firstName.match(firstNameRegexp);
+            if (checkResult[0] !== '' && checkResult.input.length <= 5) {
+                this.userDataAdd.firstName_valid = true;
+            } else {
+                this.userDataAdd.firstName_valid = false;
+            }
+        },
+        addUser_checkValid_tel() {
+            let tel = this.userDataAdd.tel;
+            let telRegexp = "^09[0-9]{8}$";
+            let checkResult = tel.match(telRegexp);
+            if (checkResult[0] !== '' && checkResult.input.length <= 10) {
+                this.userDataAdd.tel_valid = true;
+            } else {
+                this.userDataAdd.tel_valid = false;
+            }
         },
         checkPageNumbers() {
             axios.get("")
@@ -214,16 +299,29 @@ let app = new Vue({
                     console.log(this.pageNumbers);
                 })
         },
-        clearAddData() {
+        clearRemainData() {
             this.userDataAdd.lastName = "";
             this.userDataAdd.firstName = "";
             this.userDataAdd.tel = "";
             this.userDataAdd.email = "";
             this.userDataAdd.photo = "";
+            this.userDataUpdate.lastName = "";
+            this.userDataUpdate.firstName = "";
+            this.userDataUpdate.tel = "";
+            this.userDataUpdate.photo = "";
         }
     },
     created() {
         this.firstPage();
         this.checkPageNumbers();
+        var elems = document.querySelectorAll('.datepicker');
+        var instances = M.Datepicker.init(elems, options);
+    },
+    mounted() {
+        document.addEventListener('DOMContentLoaded', function () {
+            var elems = document.querySelectorAll('.datepicker');
+            M.Datepicker.init(elems, {});
+        });
+
     }
 })
