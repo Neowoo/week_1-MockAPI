6e9f2e7c30e4412a01fe286b2d5ef864a7af2aca
NeoWu <wuhongyi2011@gmail.com>
2019-05-28 20:29:37 +0800

    ok

---
+++ mms-system/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <LoginForm></LoginForm>
+    </div>
+</template>
+<script>
+import LoginForm from "@/components/Login-form.vue"
+export default {
+    name: "login-view",
+    components: {
+        LoginForm
+    }
+}
+</script>
+<style scoped>
+    /* .homepage- */
+</style>
+
